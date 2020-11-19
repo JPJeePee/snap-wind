5bca526159c42614c15265dffbb83e08c5191fdb
zhengjie <zhengjie05@kuaishou.com>
2020-11-19 22:50:30 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'snap-wind'
+  }
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  html,
+  body,
+  #app{
+    width: 100%;
+    height: 100%;
+  }
+  #app {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="wrapper">
+    <div class="row">
+      <div class=hint>请输入URL：</div>
+      <el-input class="input" v-model="url" placeholder="请输入URL"></el-input>
+    </div>
+    <div class="row">
+      <div class=hint>请输入最小点：</div>
+      <el-input class="input" v-model="minX" placeholder="请输入minX"></el-input>
+      <el-input class="input" v-model="minY" placeholder="请输入minY"></el-input>
+    </div>
+    <div class="row">
+      <div class=hint>请输入最大点：</div>
+      <el-input class="input" v-model="maxX" placeholder="请输入maxX"></el-input>
+      <el-input class="input" v-model="maxY" placeholder="请输入maxY"></el-input>
+    </div>
+    <div class="row">
+      <div class=hint>请输入精度：</div>
+      <el-input class="input" v-model="delta" placeholder="请输入delta"></el-input>
+    </div>
+    <el-button class="btn" type="primary" @click="this.handleClick">start</el-button>
+  </div>
+</template>
+
+<script>
+  const ipcRenderer = require('electron').ipcRenderer
+
+  export default {
+    name: 'landing-page',
+    data () {
+      return {
+        url: '',
+        minX: '',
+        minY: '',
+        maxX: '',
+        maxY: '',
+        delta: ''
+      }
+    },
+    methods: {
+      handleClick () {
+        // ipcRenderer.send('startPa', {
+        //   url: this.url
+        // })
+        ipcRenderer.send('test1', {a: '111'
+        })
+
+        if (this.url && this.minX && this.minY && this.maxX && this.maxY && this.delta) {
+
+        } else {
+          this.$message.error('请输入完整信息')
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  #wrapper {
+    width: 450px;
+    height: auto;
+  }
+  .row {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 10px;
+    width: 100%;
+    height: auto;
+  }
+  .hint {
+    width: 120px;
+    line-height: 40px;
+  }
+  .el-input {
+    flex: 1;
+    margin-right: 10px;
+  }
+  .el-button--primary {
+    display: block !important;
+    margin: 30px auto 0 auto !important;
+  }
+</style>
